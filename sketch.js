41e913b6ac2fc7cd2bcf2c29e615c3d8b7d6858b
B1aho <wheelsurgut00@gmail.com>
2024-09-04 22:02:48 +0300

    Add appendGrids()

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
 function createGrid() {
     const mainDiv = document.querySelector("#grid");
+    appendGrids(mainDiv);
+}
+
+function appendGrids(mainDiv) {
     for (let i = 0; i < 16; i++) {
         for (let j = 0; j < 16; j++) {
             let miniDiv = document.createElement("div");
