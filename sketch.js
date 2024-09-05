35e98c2bc619978336b92f610f67ad95ff0be53d
B1aho <wheelsurgut00@gmail.com>
2024-09-06 00:33:16 +0300

    Add setListenersForTools() and complete logic

--- sketch.js
+++ sketch.js
@@ -1,72 +1,86 @@
-// Переделать под переменные с цветом, убрать
+const mainDiv = document.querySelector("#grid");
 let currenFunction = randomColor;
-// Initilize grid and add event listeners to it and to input
+
+// Initilize grid
 function initGrid() {
-    const mainDiv = document.querySelector("#grid");
     const inputGrid = document.querySelector("#grid-input");
-    const inputColor = document.querySelector("#color-input");
-    const btnRainbow = document.querySelector("#rainbow-mode");
+    const tools = document.querySelector(".tools");
     const value = document.querySelector("#value");
-    appendGrid(mainDiv, inputGrid.value);
-    setListeners(mainDiv);
-    listenInputChangeColor(inputColor, mainDiv);
-    listenInputChangeGridValue(inputGrid, value, mainDiv);
-    listenRainbowMode(btnRainbow, mainDiv);
+    appendGrid(inputGrid.value);
+    setListenersForGrid();
+    setListenersForTools(tools);
+    listenInputChangeGridValue(inputGrid, value);
 }
 
-function setListeners (target) {
-    target.addEventListener("mousedown", (event) => {
+function setListenersForGrid() {
+    mainDiv.addEventListener("mousedown", (event) => {
         event.preventDefault();
         currenFunction(event);
-        target.addEventListener("mouseover", currenFunction)
+        mainDiv.addEventListener("mouseover", currenFunction)
     });
-    target.addEventListener("mouseup", (event) => {
-        target.removeEventListener("mouseover", currenFunction);
+    mainDiv.addEventListener("mouseup", () => {
+        mainDiv.removeEventListener("mouseover", currenFunction);
     })
 }
 
-function listenInputChangeGridValue (input, value, grid) {
+function setListenersForTools(tools) {
+    tools.addEventListener("click", (event) => {
+        console.log("tool press");
+        switch(event.target.id) {
+            case "color-input":
+            setListenersForInputColor(event.target);
+            break;
+
+            case "clear":
+            let num = mainDiv.children.length;
+            clearGrid();
+            appendGrid(num);
+            break;
+
+            case "no-grid":
+            let length = mainDiv.children.length;
+            for (let i = 0; i < length; i++) {
+                for (let j = 0; j < length; j++) {
+                    let square = mainDiv.children[i].children[j];
+                    square.classList.toggle("square-no-outline");
+                }
+            }
+            break;
+
+            case "rainbow-mode":
+            currenFunction = randomColor;
+            break;
+
+            default:
+            break;
+        }
+    })
+}
+
+function listenInputChangeGridValue (input, value) {
     input.addEventListener("input", (event) => {
         let inputVal = event.target.value;
         value.textContent = `${inputVal} X ${inputVal}`;
-        changeGrid(inputVal, grid);
+        changeGrid(inputVal);
     })
 }
 
-function listenRainbowMode (btn, target) {
-    btn.addEventListener("click", () => {currenFunction = randomColor;})
+function changeGrid(num) {
+    clearGrid();
+    appendGrid(num);
 }
 
-// Parse input-color value and change pen to a new chosen color
-function listenInputChangeColor (inputColor, mainDiv) {
-    inputColor.addEventListener("input", (event) => {
-        let color = event.target.value;
-        let red = parseInt(color.substr(1, 2), 16);
-        let green = parseInt(color.substr(3, 2), 16);
-        let blue = parseInt(color.substr(5, 2), 16);
-        let colorFunc = (r, g, b) => function (event) {
-            event.target.style.background = `rgb(${r}, ${g} ,${b})`;
-        }
-        currenFunction = colorFunc(red, green, blue);
-    })
-}
-
-function changeGrid(num, grid) {
-    clearGrid(grid)
-    appendGrid(grid, num);
-}
-
-function clearGrid(grid) {
-    while (grid.firstChild) {
-        grid.removeChild(grid.lastChild);
+function clearGrid() {
+    while (mainDiv.firstChild) {
+        mainDiv.removeChild(mainDiv.lastChild);
     }
 }
 
-function appendGrid(parentNode, squareNumber) {
+function appendGrid(squareNumber) {
     for (let i = 0; i < squareNumber; i++) {
         let column = document.createElement("div");
         column.setAttribute("class", "column");
-        parentNode.appendChild(column);
+        mainDiv.appendChild(column);
         for (let j = 0; j < squareNumber; j++) {
             let square = document.createElement("div");
             square.setAttribute("class", "square");
@@ -80,6 +94,19 @@ function randomColor(event) {
     let y = Math.floor(Math.random() * 255 + 1);
     let z = Math.floor(Math.random() * 255 + 1);
     event.target.style.background = `rgb(${x}, ${y} ,${z})`; 
+}
+
+function setListenersForInputColor(input) {
+    input.addEventListener("input", (event) => {
+        let color = event.target.value;
+        let red = parseInt(color.substr(1, 2), 16);
+        let green = parseInt(color.substr(3, 2), 16);
+        let blue = parseInt(color.substr(5, 2), 16);
+        let colorFunc = (r, g, b) => function (event) {
+            event.target.style.background = `rgb(${r}, ${g} ,${b})`;
+        }
+        currenFunction = colorFunc(red, green, blue);
+    })
 }
 
 initGrid();
